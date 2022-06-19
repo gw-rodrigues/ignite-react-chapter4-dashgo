629956d51e8a1a49395225b3663768c58ec13b64
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-19 15:48:10 +0200

    UPDT add search box

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Input, Text, Icon } from "@chakra-ui/react";
+import { RiSearchLine } from "react-icons/ri";
 export function Header() {
   return (
     <Flex
@@ -17,6 +18,30 @@ export function Header() {
           .
         </Text>
       </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          px="4"
+          mr="4"
+          placeholder="Buscar na plataforma"
+          _placeholder={{ color: "gray.400" }}
+        />
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
     </Flex>
   );
 }
