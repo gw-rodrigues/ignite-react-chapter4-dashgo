99809d03d489c7c31051b5c92d1f652e973a1f66
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-19 17:24:33 +0200

    IMPORT sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,14 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 }
