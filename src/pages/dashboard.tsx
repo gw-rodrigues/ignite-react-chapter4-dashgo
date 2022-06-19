f7ad91927035ea20c42e1d0fc43f262b51321fb3
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-19 17:45:14 +0200

    UPDT style

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <Flex>
+    <Flex direction="column" h="100vh">
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
