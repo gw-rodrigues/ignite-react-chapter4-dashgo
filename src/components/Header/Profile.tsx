39a11d190684e46e515995a61d60d69edbe5e1e0
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-21 12:02:06 +0200

    UPDT header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gleyson Rodrigues</Text>
-        <Text color="gray.300" fontSize="small">
-          rorigues.gw@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gleyson Rodrigues</Text>
+          <Text color="gray.300" fontSize="small">
+            rorigues.gw@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Gleydson Rodrigues"
