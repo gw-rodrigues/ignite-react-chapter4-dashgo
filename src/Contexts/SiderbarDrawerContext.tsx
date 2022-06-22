0c849bde4c8bf1b5886c90d3b5f76a0af66e2459
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-22 22:57:48 +0200

    UPDT function close nav menu

--- src/Contexts/SiderbarDrawerContext.tsx
+++ src/Contexts/SiderbarDrawerContext.tsx
@@ -1,5 +1,6 @@
 import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
-import { createContext, ReactNode, useContext } from "react";
+import { useRouter } from "next/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
 
 interface SidebarDrawerProviderProps {
   children: ReactNode;
@@ -13,6 +14,12 @@ export function SidebarDrawerProvider({
   children,
 }: SidebarDrawerProviderProps) {
   const disclouse = useDisclosure();
+  const router = useRouter();
+
+  useEffect(() => {
+    disclouse.onClose();
+  }, [router.asPath]);
+
   return (
     <SidebarDrawerContext.Provider value={disclouse}>
       {children}
