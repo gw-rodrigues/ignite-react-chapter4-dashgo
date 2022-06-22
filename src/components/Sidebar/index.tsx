02e4ff5f5b8a23398069fe74245ca07b69b0af30
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-22 22:58:39 +0200

    UPDT add function open and close

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -8,9 +8,12 @@ import {
   DrawerOverlay,
   useBreakpointValue,
 } from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../Contexts/SiderbarDrawerContext";
 import { SiderbarNav } from "./SiderbarNav";
 
 export function Sidebar() {
+  const { isOpen, onClose } = useSidebarDrawer();
+
   const isDrawerSiderbar = useBreakpointValue({
     base: true,
     lg: false,
@@ -18,7 +21,7 @@ export function Sidebar() {
 
   if (isDrawerSiderbar) {
     return (
-      <Drawer isOpen={true} placement="left" onClose={() => {}}>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
         <DrawerOverlay w="100%" h="100%">
           <DrawerContent bg="gray.800" p="4">
             <DrawerCloseButton />
