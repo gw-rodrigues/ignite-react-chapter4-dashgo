5caa57a8c7454fb570c44c3fee9cd18e32bad3eb
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-27 23:29:55 +0200

    UPDT using reacthooksform to data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
-import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Button, Flex, FormErrorMessage, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    //resolver: yupResolver(signInFormSchema),
+  });
+  //formState , form esta validados, informacoes, erros, em submit, etc.
+  const { errors } = formState;
+
+  const handleSignin: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" height="100vh" align="center" justify="center">
       <Flex
@@ -11,12 +29,31 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignin)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            //error={errors.email}
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            //error={errors.email}
+            {...register("password")}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
