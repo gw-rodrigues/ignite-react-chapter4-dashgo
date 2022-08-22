b104feeb3f11a153160ba1426f8ff8b6c861e7f8
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-22 20:38:05 +0200

    UPDT requesting and processing data from mirage

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,18 +13,30 @@ import {
   Td,
   Text,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query"; //requisições para back-end
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  //os dados do react-query vao ficar armazenados dentro cache na nossa aplicação
+  //assim se precisar dos dados num intervalo de tempo nao precisa pedir ao backend
+  //criando user query com nome da cache "users"
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const res = await fetch("http://localhost:3000/api/users");
+    const data = await res.json();
+    return data;
+  });
+
   const isWiderVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Box>
       <Header />
@@ -49,102 +61,114 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usários</Th>
-                {isWiderVersion && <Th>Data de cadastro</Th>}
-                {isWiderVersion && <Th width="8"></Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gleydson Rodrigues</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      Gleydson Rodrigues
-                    </Text>
-                  </Box>
-                </Td>
-                {isWiderVersion && <Td>20 de Junho, 2022</Td>}
-                {isWiderVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gleydson Rodrigues</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      Gleydson Rodrigues
-                    </Text>
-                  </Box>
-                </Td>
-                {isWiderVersion && <Td>20 de Junho, 2022</Td>}
-                {isWiderVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Gleydson Rodrigues</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      Gleydson Rodrigues
-                    </Text>
-                  </Box>
-                </Td>
-                {isWiderVersion && <Td>20 de Junho, 2022</Td>}
-                {isWiderVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter os dados do usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usários</Th>
+                    {isWiderVersion && <Th>Data de cadastro</Th>}
+                    {isWiderVersion && <Th width="8"></Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Gleydson Rodrigues</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          Gleydson Rodrigues
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWiderVersion && <Td>20 de Junho, 2022</Td>}
+                    {isWiderVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Gleydson Rodrigues</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          Gleydson Rodrigues
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWiderVersion && <Td>20 de Junho, 2022</Td>}
+                    {isWiderVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Gleydson Rodrigues</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          Gleydson Rodrigues
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWiderVersion && <Td>20 de Junho, 2022</Td>}
+                    {isWiderVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
