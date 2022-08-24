06d50d7b665eda20380726f7421ebccbeea613ab
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-24 21:05:44 +0200

    UPDT removing useQuery and api, creating hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,37 +17,16 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from "react-query"; //requisições para back-end
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
   //os dados do react-query vao ficar armazenados dentro cache na nossa aplicação
   //assim se precisar dos dados num intervalo de tempo nao precisa pedir ao backend
   //criando user query com nome da cache "users"
-  const { data, isLoading, isFetching, error } = useQuery(
-    "users",
-    async () => {
-      const { data } = await api.get("users");
-
-      const users = data.users.map((user) => ({
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      }));
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, //5 segundos, que o react-query nao ira re-validar as informações, nem atualizar a página
-    }
-  );
+  const { data, isLoading, isFetching, error } = useUsers();
 
   const isWiderVersion = useBreakpointValue({
     base: false,
