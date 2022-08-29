0a789299b6bf9084cc68c50cb1ed81395db14f82
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-29 11:57:26 +0200

    UPDT add prefetch user on mouseHover

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,24 +14,40 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
+  Link,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
+  const [page, setPage] = useState(1);
   //os dados do react-query vao ficar armazenados dentro cache na nossa aplicação
   //assim se precisar dos dados num intervalo de tempo nao precisa pedir ao backend
   //criando user query com nome da cache "users"
-  const { data, isLoading, isFetching, error } = useUsers();
+  const { data, isLoading, isFetching, error } = useUsers(page); //envia pagina atual para hooks useUsers
 
   const isWiderVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(
+      ["user", userId],
+      async () => {
+        const response = await api.get(`users/${userId}`); //sempre for buscar 1 valor usar miragejs "this.get(...)"
+        return response.data;
+      },
+      { staleTime: 1000 * 60 * 10 } //10 minutos que dados nao sera atualizados/ou fresh (estarão stale)
+    );
+  }
 
   return (
     <Box>
@@ -47,7 +63,7 @@ export default function UserList() {
                 <Spinner size="sm" color="gray.500" ml="5" />
               )}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -57,7 +73,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -82,7 +98,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -90,7 +106,12 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <Link
+                              color="purple.300"
+                              onMouseEnter={() => handlePrefetchUser(user.id)}
+                            >
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">
                               {user.email}
                             </Text>
@@ -118,9 +139,9 @@ export default function UserList() {
                 </Tbody>
               </Table>
               <Pagination
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}
