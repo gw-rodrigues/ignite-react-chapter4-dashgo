3cf41a367b2ac847551f7caa3a019696e447b54b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-29 11:18:47 +0200

    UPDT moving uqeryClient to hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
 import { ReactQueryDevtools } from "react-query/devtools";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
 
 import { SidebarDrawerProvider } from "../Contexts/SiderbarDrawerContext";
 import { makeServer } from "../services/mirage";
+import { queryClient } from "../services/queryClient";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
