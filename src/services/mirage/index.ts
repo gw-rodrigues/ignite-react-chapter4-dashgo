2d114b32afd1fa582527fb70a1be073957050f9c
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-29 10:49:55 +0200

    UPDT creating per page schema

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs"; //server api
+import { createServer, Factory, Model, Response } from "miragejs"; //server api
 import { faker } from "@faker-js/faker"; //geração dados fictícios
 
 type User = {
@@ -32,14 +32,32 @@ export function makeServer() {
 
     seeds(server) {
       //criar usuários ou dados assim mirage for iniciado
-      server.createList("user", 10); //criar uma lista do modele user, de 200 usuários
+      server.createList("user", 200); //criar uma lista do modele user, de 200 usuários
     },
 
     routes() {
       this.namespace = "api"; //definir a rota /api/users
       this.timing = 750; //tempo de request
 
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+        const totalUsers = schema.all("user").length; //recebe total users existentes
+
+        const pageStart = (Number(page) - 1) * Number(per_page); //ex.: page: 3 - 1 = 2 * 10 = 20 - inicia item 20
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        ); //retorna objeto com array uses ex.: { ..., user:[user1,user2,...],...} / usar serialize para usar o nosso model acima, se da error.
+
+        //como nosso requisição nao faz parte da estrutura, precisamos enviar como headers
+        return new Response(
+          200,
+          { "x-total-count": String(totalUsers) },
+          { users }
+        ); //retornamos 200 success, nome padrão usado comunidade 'x-total-count' com total users, e os registros users - listagem
+      });
       this.post("/users");
 
       this.namespace = ""; //para nao dar conflito com rotas api do Next, do src/api
