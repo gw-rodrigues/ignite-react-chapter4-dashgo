319e0011c1d6e5a7da62283381efa17ff1ed9166
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-30 09:25:15 +0200

    UPDT fixing type of userId

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query"; //requisições para back-end
 import { api } from "../api";
 
 type User = {
-  id: number;
+  id: string;
   name: string;
   email: string;
   createdAt: string;
